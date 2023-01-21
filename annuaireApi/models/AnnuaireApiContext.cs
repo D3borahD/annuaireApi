038d7eee739e82ee311a2c9f23d819f8055ff81a
Deborah <deborah.douillet@aum.bio>
2023-01-21 11:19:38 +0100

    initializzed project

---
+++ annuaireApi/models/AnnuaireApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using annuaireApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace annuaireApi.models
+{
+	public class AnnuaireApiContext:DbContext
+	{
+		public AnnuaireApiContext(DbContextOptions<AnnuaireApiContext> options)
+			:base(options)
+		{
+
+		}
+
+		public DbSet<Site> Sites{ get; set; }
+
+	}
+}
+
---
+++ annuaireApi/models/Site.cs
@@ -0,0 +1,13 @@
+﻿
+namespace annuaireApi.Models
+{
+	public class Site
+	{
+		
+		public int id { get; set; }
+		public string name { get; set; }
+
+		
+	}
+}
+
